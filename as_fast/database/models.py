e4b8a604906d4c767cc05d592685e14526754240
VladSkopenko <skopirka2k17@gmail.com>
2024-03-26 14:39:37 +0300

    + models

---
+++ as_fast/database/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, DateTime
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    ...
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50), index=True)
+    second_name: Mapped[str] = mapped_column(String(50), index=True)
+    email_add: Mapped[str] = mapped_column(String(100), unique=True, index=True)
+    phone_num: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    birth_date: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
